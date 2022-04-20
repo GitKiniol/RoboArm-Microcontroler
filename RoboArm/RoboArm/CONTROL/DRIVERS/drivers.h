8d5b11247997de7378c45e28b13f59530f0c19cd
Kiniol <kiniolandroid@gmail.com>
2022-04-20 17:52:20 +0200

    Dodanie struktury driver_t

--- RoboArm/RoboArm/CONTROL/DRIVERS/drivers.h
+++ RoboArm/RoboArm/CONTROL/DRIVERS/drivers.h
@@ -17,7 +17,19 @@
 
 /*-----------------------------------------------Definicje struktur danych--------------------------------------------------*/
 
-
+typedef struct DRIVER_STRUCT
+{
+	uint16_t SetpointPosition;								/* pozycja zadana												*/
+	uint16_t CurrentPosition;								/* aktualna pozycja												*/
+	uint8_t Direction;										/* kierunek ruchu												*/
+	TC1_t *DriverTimer;										/* timer sterownika												*/
+	PORT_t *DriverPort;										/* port pinów sterownika										*/
+	uint8_t EnablePin;										/* numer pinu dla sygna³u enable								*/
+	uint8_t DirectionPin;									/* numer pinu dla sygna³u dir									*/
+	uint8_t PulsePin;										/* numer pinu dla sygna³u pul									*/
+	uint8_t IsRunning:1;									/* flaga informuj¹ca o pracy/zatrzymaniu sterownika				*/
+	
+}driver_t;
 
 /*--------------------------------------------------------------------------------------------------------------------------*/
 
