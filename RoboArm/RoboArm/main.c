023da573f1ea1d87cde48a34b4ccb97f8106fa16
kiniol desktop <kiniolandroid@gmail.com>
2022-08-09 13:32:02 +0200

    ROB-110 testy zakończone, usunięcie kodu testów

--- RoboArm/RoboArm/main.c
+++ RoboArm/RoboArm/main.c
@@ -21,10 +21,6 @@
 #include "CONTROL/DRIVERS/drivers.h"
 
 TWI_t *lcdBus = &TWIC;
-to_run_list_t *test_lista;
-to_run_drv_t *test_toRunDrv;
-servo_driver_t test_servo1, test_servo2;
-stepper_driver_t test_stepper1, test_stepper2;
 
 int main(void)
 {
@@ -35,24 +31,10 @@ int main(void)
 	Job = Data_CreateList();
 	Bluetooth = HC05_Init(Bluetooth);
 	
-	/*test block 1*/
-	test_lista = Driver_ToRunListInit();
-	/*end test block 1*/
-	
 	sei();
     while (1) 
     {
 		Bluetooth->Read();						/* cykliczne odbieranie ramek danych z telefonu	*/
-		/*test block 2*/
-		Driver_ToRunListAdd(test_lista, &test_servo1, SERVO);
-		Driver_ToRunListAdd(test_lista, &test_stepper1, STEPPER);
-		Driver_ToRunListAdd(test_lista, &test_servo2, SERVO);
-		Driver_ToRunListAdd(test_lista, &test_stepper1, STEPPER);
-		test_toRunDrv = Driver_ToRunListGet(test_lista);
-		test_toRunDrv = Driver_ToRunListGet(test_lista);
-		test_toRunDrv = Driver_ToRunListGet(test_lista);
-		test_toRunDrv = Driver_ToRunListGet(test_lista);
-		/*end test block 2*/
     }
 }
 
